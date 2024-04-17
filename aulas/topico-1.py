ef4778f2818e2958e3ac9ac69fbd7265cd0e9833
AYAMzzzy <ayamgilmarto16@gmail.com>
2024-04-17 15:18:29 -0300

    novas pastas

--- aulas/topico-1.py
+++ aulas/topico-1.py
@@ -13,4 +13,11 @@ print("a variavel e do tipo ",type(z)) # informa o tipo de dado
 i = 1
 g = "123"
 j = 2.9
-print(str(i) + str(i))
+print(str(i) + str(i))# int(a) muda a varivel a para int .casting concerte o tipo de var
+''' variveis tem que ser declaradas com nomes e descrição do tipo de dados 
+que elas representa. ex: "a = 32"  varivel a pode significar varias coisas
+se for idade seria melhor representar assim idade = 21, seria mais facil
+de comprender
+
+variaveis globais = fora de uma funçao sempre fica no pc
+variavris locais = sempre existem dentro dessa função '''
