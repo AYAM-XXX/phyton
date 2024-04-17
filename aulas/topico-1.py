e10151e190d017a4005bbbd91a84a2647b068f78
AYAMzzzy <ayamgilmarto16@gmail.com>
2024-04-17 15:10:28 -0300

    priemreiro update

---
+++ aulas/topico-1.py
@@ -0,0 +1,16 @@
+#variaveis
+a = 1
+b = "e maneiro" 
+w = "  ayam e brabo  "
+c = w+b # junta as variaveis
+z = 1+2j
+print(w.strip()) # tira os  espaços da beirada da string ex : " amor " passa a ficar "amor"
+print(c)
+print(w.upper()) # deixa as letras tudo em maiusculas
+print(w.lower()) # deixa tudo em minuscula
+print("a variavel e do tipo ",type(z)) # informa o tipo de dado
+# como transformar uma varivel de qualquer tipo para outro tipo
+i = 1
+g = "123"
+j = 2.9
+print(str(i) + str(i))
