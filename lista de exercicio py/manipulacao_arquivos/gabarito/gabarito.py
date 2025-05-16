4aef7ee462e3acf9a2dd31a68ba47be32ea58b64
ayam <ayamgilmarto16@gmail.com>
2025-05-15 23:17:20 -0300

    script para autoescola

---
+++ lista de exercicio py/manipulacao_arquivos/gabarito/gabarito.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+
+nome_arquivo = input("digite o nome da prova: ") + '.txt'
+pasta = Path('manipulação de arquivo/preencher gabarito/provas')
+arquivo = pasta / nome_arquivo
+arquivo.touch()
+
+
+def preencher():
+    resposta = ''
+    for i in range(1, 31):
+        print(f"questão {i}")
+        gabarito = input("digite a opcão: ")
+        resposta += f"{i}- {gabarito}\n"
+        print("\n")
+    arquivo.write_text(resposta)
+
+
+preencher()
+print(arquivo.read_text())
