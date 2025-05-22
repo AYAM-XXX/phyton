1348d61018a6c3cc56900bd012e8e3c112b6f665
ayam <ayamgilmarto16@gmail.com>
2025-05-21 21:02:36 -0300

    testando

--- desktop.py
+++ desktop.py
@@ -1,3 +1,16 @@
-def get_formatted_name(first, last):
-    full_name = f"{first} {last}"
+def get_formatted_name(first, middle, last):
+    full_name = f"{first} {middle} {last}"
     return full_name.title()
+
+
+if __name__ == "__main__":
+    print("Enter 'q' at any time to quit.")
+    while True:
+        first = input("\nPlease give me a first name: ")
+        if first == 'q':
+            break
+        last = input("Please give me a last name: ")
+        if last == 'q':
+            break
+        formatted_name = get_formatted_name(first, last)
+        print(f"\tNeatly formatted name: {formatted_name}.")
